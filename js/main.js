4e081bbefe84474b9b0ab9e2df1c7f3e89df3676
aguzpos332 <aguzpos332@g.educaand.es>
2026-03-03 12:37:05 +0100

    Pequeños fix y pre-estructura del js, falta completar la verificacion de formularios

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const sideMenu = document.querySelector("#sideMenu");
 const toggleBtn = document.querySelector(".open-menu");
 const toggleIcon = document.querySelector("#toggleIcon"); 
+const formRegistro = document.querySelector("#registro");
+const formPatrocinio = document.querySelector("#patrocinio");
     toggleBtn.addEventListener("click", () => {
         sideMenu.classList.toggle("active");
         toggleBtn.classList.toggle("active");
@@ -10,3 +12,16 @@ const toggleIcon = document.querySelector("#toggleIcon");
             toggleIcon.textContent = "☰";
         }
     });
+    formRegistro.addEventListener("submit",function (event){
+        event.preventDefault();
+        if (verificarFormulario()){
+        }
+    });
+    formPatrocinio.addEventListener("submit",function (event){
+        event.preventDefault();
+        if (verificarFormulario()){
+        }
+    });
+    function verificarFormulario(){
+        
+    }
